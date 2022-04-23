ff25932d2379e3af7e01d846d3df77c62f1bec60
Charanvir <charanvir123@gmail.com>
2022-04-22 20:43:13 -0400

    added imola quali scores and added sprint section to necessary tables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 // Overall
 points = [
-    { name: "Charanvir", score: "152" },
-    { name: "Gaganvir", score: "82" },
-    { name: "Gurvir", score: "102" },
-    { name: "Jaskaran", score: "88" },
-    { name: "Joe", score: "47" },
-    { name: "Manroop", score: "69" },
-    { name: "Randeep", score: "47" }
+    { name: "Charanvir", score: "165" },
+    { name: "Gaganvir", score: "96" },
+    { name: "Gurvir", score: "119" },
+    { name: "Jaskaran", score: "103" },
+    { name: "Joe", score: "52" },
+    { name: "Manroop", score: "83" },
+    { name: "Randeep", score: "58" }
 ];
 let overallStandings = points.sort((a, b) => b.score - a.score);
 let overallBody = document.querySelector(".overallBody")
@@ -32,26 +32,26 @@ let overallScores = function () {
 // Drivers Overall Standings
 drivers = [
     // Charanvir
-    { name: "Leclerc", quali: "44", race: "56", score: "100" },
-    { name: "Ocon", quali: "19", race: "33", score: "52" },
+    { name: "Leclerc", quali: "58", sprint: "0", race: "56", score: "114" },
+    { name: "Ocon", quali: "18", sprint: "0", race: "33", score: "51" },
     // Randeep
-    { name: "Hamilton", quali: "21", race: "34", score: "55" },
-    { name: "Alonso", quali: "15", race: "-23", score: "-8" },
+    { name: "Hamilton", quali: "21", sprint: "0", race: "34", score: "55" },
+    { name: "Alonso", quali: "26", sprint: "0", race: "-23", score: "3" },
     // Gaganvir
-    { name: "Perez", quali: "32", race: "9", score: "41" },
-    { name: "Bottas", quali: "22", race: "19", score: "41" },
+    { name: "Perez", quali: "38", sprint: "0", race: "9", score: "47" },
+    { name: "Bottas", quali: "30", sprint: "0", race: "19", score: "49" },
     // Jaskaran
-    { name: "Verstappen", quali: "36", race: "10", score: "46" },
-    { name: "Stroll", quali: "-2", race: "44", score: "42" },
+    { name: "Verstappen", quali: "51", sprint: "0", race: "10", score: "61" },
+    { name: "Stroll", quali: "-2", sprint: "0", race: "44", score: "42" },
     // Manroop
-    { name: "Sainz", quali: "21", race: "10", score: "31" },
-    { name: "Magnussen", quali: "14", race: "24", score: "38" },
+    { name: "Sainz", quali: "23", sprint: "0", race: "10", score: "33" },
+    { name: "Magnussen", quali: "26", sprint: "0", race: "24", score: "50" },
     // Joe
-    { name: "Gasly", quali: "17", race: "14", score: "31" },
-    { name: "Ricciardo", quali: "4", race: "12", score: "16" },
+    { name: "Gasly", quali: "16", sprint: "0", race: "14", score: "30" },
+    { name: "Ricciardo", quali: "10", sprint: "0", race: "12", score: "22" },
     // Gurvir
-    { name: "Norris", quali: "20", race: "18", score: "38" },
-    { name: "Russel", quali: "19", race: "45", score: "64" },
+    { name: "Norris", quali: "33", sprint: "0", race: "18", score: "51" },
+    { name: "Russel", quali: "23", sprint: "0", race: "45", score: "68" },
 
 
 ]
@@ -65,10 +65,12 @@ let driverScores = function () {
         let header = document.createElement("th");
         let teamName = document.createElement("td");
         let qualiScore = document.createElement("td");
+        let sprintScore = document.createElement("td");
         let raceScore = document.createElement("td");
         let teamScore = document.createElement("td");
         teamName.innerHTML = driverStandings[i].name
         qualiScore.innerHTML = driverStandings[i].quali
+        sprintScore.innerHTML = driverStandings[i].sprint
         raceScore.innerHTML = driverStandings[i].race
         teamScore.innerHTML = driverStandings[i].score
         header.setAttribute("scope", "row");
@@ -76,6 +78,7 @@ let driverScores = function () {
         row.appendChild(header);
         row.appendChild(teamName);
         row.appendChild(qualiScore);
+        row.appendChild(sprintScore);
         row.appendChild(raceScore);
         row.appendChild(teamScore);
         driverBody.appendChild(row);
@@ -202,7 +205,7 @@ australia = [
     { name: "Pierre Gasly", quali: 4, race: 10, score: 14 },
     { name: "Daniel Ricciardo", quali: 5, race: 5, score: 10 },
     // Gaganvir 43
-    { name: "Sergio Perez", quali: 9, race: 15, score: 24 },
+    { name: "Sergio Perez", quali: 10, race: 15, score: 25 },
     { name: "Valtteri Bottas", quali: 4, race: 15, score: 19 },
     // Gurvir 44
     { name: "George Russel", quali: 6, race: 18, score: 24 },
@@ -235,6 +238,60 @@ let australiaStandingsFunction = function () {
     }
 }
 
+// Imola Standings
+imola = [
+    // Randeep  11
+    { name: "Lewis Hamilton", quali: 0, sprint: 0, race: 0, score: 0 },
+    { name: "Fernando Alonso", quali: 11, sprint: 0, race: 0, score: 11 },
+    // Jaskaran 
+    { name: "Max Verstappen", quali: 15, sprint: 0, race: 0, score: 15 },
+    { name: "Lance Stroll", quali: 0, sprint: 0, race: 0, score: 0 },
+    // Manroop 
+    { name: "Carlos Sainz", quali: 2, sprint: 0, race: 0, score: 2 },
+    { name: "Kevin Magnussen", quali: 12, sprint: 0, race: 0, score: 12 },
+    // Charanvir 
+    { name: "Charles Leclerc", quali: 14, sprint: 0, race: 0, score: 14 },
+    { name: "Esteban Ocon", quali: -1, sprint: 0, race: 0, score: -1 },
+    // Joe 
+    { name: "Pierre Gasly", quali: -1, sprint: 0, race: 0, score: -1 },
+    { name: "Daniel Ricciardo", quali: 6, sprint: 0, race: 0, score: 6 },
+    // Gaganvir 
+    { name: "Sergio Perez", quali: 5, sprint: 0, race: 0, score: 5 },
+    { name: "Valtteri Bottas", quali: 8, sprint: 0, race: 0, score: 8 },
+    // Gurvir 
+    { name: "George Russel", quali: 4, sprint: 0, race: 0, score: 4 },
+    { name: "Lando Norris", quali: 13, sprint: 0, race: 0, score: 13 }
+];
+let imolaStandings = imola.sort((a, b) => b.score - a.score);
+let imolaBody = document.querySelector(".imolaBody")
+let imolaStandingsFunction = function () {
+    counter = 0;
+    for (i = 0; i < imolaStandings.length; i++) {
+        counter++
+        let row = document.createElement("tr")
+        let header = document.createElement("th");
+        let teamName = document.createElement("td");
+        let qualiScore = document.createElement("td");
+        let sprintScore = document.createElement('td');
+        let raceScore = document.createElement("td");
+        let teamScore = document.createElement("td");
+        teamName.innerHTML = imolaStandings[i].name
+        qualiScore.innerHTML = imolaStandings[i].quali
+        sprintScore.innerHTML = imolaStandings[i].sprint
+        raceScore.innerHTML = imolaStandings[i].race
+        teamScore.innerHTML = imolaStandings[i].score
+        header.setAttribute("scope", "row");
+        header.innerHTML = counter;
+        row.appendChild(header);
+        row.appendChild(teamName);
+        row.appendChild(qualiScore);
+        row.appendChild(sprintScore)
+        row.appendChild(raceScore);
+        row.appendChild(teamScore);
+        imolaBody.appendChild(row);
+    }
+}
+
 
 
 overallScores();
@@ -242,3 +299,4 @@ driverScores();
 bahrainStandingsFunction();
 saudiStandingsFunction();
 australiaStandingsFunction();
+imolaStandingsFunction();
